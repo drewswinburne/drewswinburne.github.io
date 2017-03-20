3ea894d78fe458e5273322bfe387469e4c9fedcf
Drew Swinburne <drewswinburne@gmail.com>
2017-03-20 15:04:04 -0400

    some fun stuff

--- main.js
+++ main.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
 
+//Headline flash
+
+// const selected = document.querySelectorAll('.marqee');
+// selected.forEach(item => item.addEventListener('click', changeColor))
+
+window.onload = function(){
+  const marqueeText = "DREW SWINBURNE"
+  const marqueeArray = marqueeText.split("")
+
+    for (var i = 0;i<marqueeArray.length;i++){
+      function addALetter(i) {
+        setTimeout(function() {
+          var item = document.createElement("span")
+          var div = document.querySelector("h1")
+          item.className = "noselect"
+          item.colorNumber = 0
+          item.onclick = changeColor;
+          item.innerHTML = marqueeArray[i];
+          div.appendChild(item)
+        }, i * 75);
+      }
+      addALetter(i)
+    }
+
+    function changeColor(){
+      let colors = ["DarkTurquoise", "DeepPink", "Yellow"]
+      let color = colors[this.colorNumber]
+      this.colorNumber++;
+      if (this.colorNumber > colors.length){
+        this.colorNumber = 0;
+      }
+    this.style.setProperty('color', color)
+    }
+}
+
+//smooth scroll
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -23,7 +59,7 @@ navButtons.forEach(button => button.addEventListener('click', flashColor))
 
   function flashColor(){
     $(this).css("color", "blue");
-    setTimeout(colorBack, 100);
+    setTimeout(colorBack, 400);
     function colorBack(){
       $('.navbutton').css("color", "white")
     }
